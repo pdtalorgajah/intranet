243c4a1ff56723cefc3cb8869c339ddf1c97b269
Md Ridzuan bin Mohammad Latiah <md.ridzuan80@gmail.com>
2025-07-03 15:27:55 +0800

    create layout for dashboard

---
+++ resources/views/dashboard/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
+
+        <div class="row">
+            <div class="col-lg-8">
+                <!-- Content Row -->
+                <div class="card shadow mb-4">
+                    <div class="card-header py-3">
+                        <h6 class="m-0 font-weight-bold text-primary">Basic Card Example</h6>
+                    </div>
+                    <div class="card-body">
+                        The styling for this basic card example is created by using default Bootstrap
+                        utility classes. By using utility classes, the style of the card component can be
+                        easily modified with no need for any custom CSS!
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
